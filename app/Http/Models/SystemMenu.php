e806bc9a1015ae7e686b5b652209f9a1be787b58
zhw <1573529065@qq.com>
2019-05-23 19:52:46 +0800

    RBAC

---
+++ app/Http/Models/SystemMenu.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 2019/5/23
+ * Time: 19:50
+ */
+
+namespace App\Http\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class SystemMenu extends Model
+{
+
+}
